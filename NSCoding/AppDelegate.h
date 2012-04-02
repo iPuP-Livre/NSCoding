22f0ffae57e1168a46ac46344dca5d0d99250457
ipodishima <marian.paul2@gmail.com>
2012-04-02 15:30:57 +0200

    Initial Commit

---
+++ NSCoding/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  NSCoding
+//
+//  Created by Marian Paul on 02/04/12.
+//  Copyright (c) 2012 iPuP SARL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
